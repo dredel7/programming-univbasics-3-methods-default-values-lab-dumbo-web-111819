08e08e0d470486c8a3d0dba10fe351077a741020
Andrae Delisser <delisserad@gmail.com>
2019-10-19 19:08:03 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 def meal_choice(veg1, veg2, protein="meat")
   puts "What a nutritious meal!"
 
-  puts "A plate of #{meat} with #{carrot} and #{beats}"
+  puts "A plate of #{meat} with #{carrot} and #{beats}."
   
 end
    meal_choice
